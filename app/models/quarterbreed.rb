c0ce1f919f818e85904d3081e0c08f2ad5483668
Amanda Lemmons <amandalemmons@Amandas-MBP.home>
2015-10-27 17:26:39 -0500

    chagnes

--- app/models/quarterbreed.rb
+++ app/models/quarterbreed.rb
@@ -1,5 +1,5 @@
 class Quarterbreed < ActiveRecord::Base
-    validates :jaguar_vin, :car_owner_full_name, :car_owner_address, :car_owner_city, :car_owner_state, :car_owner_zip, presence: true
+    validates :jaguar_vin, :car_owner_full_name, :car_owner_address, :car_owner_city, :car_owner_state, :car_owner_zip, :same_information, :credit, presence: true
 
     def set_same_shipping
       if self.same_information == true
