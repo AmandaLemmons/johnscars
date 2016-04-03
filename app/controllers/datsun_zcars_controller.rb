4a95ad5fe96123c6c144269862aa7d35d45ca286
Amanda Lemmons <amanda.lemmons@gmail.com>
2016-04-03 17:25:06 +0200

    model forumlas

--- app/controllers/datsun_zcars_controller.rb
+++ app/controllers/datsun_zcars_controller.rb
@@ -57,21 +57,29 @@ class DatsunZcarsController < ApplicationController
    params.require(:datsun_zcar).permit(:car_owner_full_name, :car_owner_address, :car_owner_city, :car_owner_state, :car_owner_email, :car_owner_zip, :car_owner_home_phone, :car_owner_work_phone, :car_owner_fax, :car_owner_find_us, :car_owner_home_hours, :car_owner_work_hours, :car_owner_fax_hours, :car_owner_country, :car_owner_where_find_us, :ship_to_full_name, :ship_to_address, :ship_to_city, :ship_to_state, :ship_to_email, :ship_to_zip, :ship_to_phone, :ship_to_fax, :ship_to_contact_person, :ship_to_country, :same_information, :comments, :type, :vin, :year, :month_built, :year_built, :engine, :rear_axle, :tire_size, :power_steering, :zcar_kit, :tach_calibration, :tach_calibration_without_smog, :ramshorn_w_smog, :ramshorn_w_egr_smog, :driveshaft_except_t400, :driveshaft_t400, :radiator, :electric_fan, :electrolock_t700, :other, :credit_amt, :tach_calibration_amt, :tach_calibration_without_smog_amt, :ramshorn_w_smog_amt, :ramshorn_w_egr_smog_amt, :driveshaft_except_t400_amt, :driveshaft_t400_amt, :radiator_amt, :electric_sfan_amt, :electrolock_t700_amt, :other_amt, :credit_description, :subtotal, :expedite, :sales_tax, :total, :driveshaft_adaptor, :driveshaft_adaptor_amt, :rear_axl_ratio, :trans_type)
  end
 
- # def set_datsun_zcar
- #    @datsun_zcar = EtypeV12.find(params[:id])
- #    @datsun_zcar.set_same_shipping
- #    @datsun_zcar.set_auto_trans_kit
- #    @datsun_zcar. set_manual_tras_kit
- #    @datsun_zcar.set_tach_calibration
- #    @datsun_zcar.set_fuel_injection_v8
- #    @datsun_zcar.set_overdrive
- #    @datsun_zcar.set_subtotal
- #    @datsun_zcar.set_sales_tax
- #    @datsun_zcar.set_total
- #    @datsun_zcar.set_home_phone
- #    @datsun_zcar.set_work_phone
- #    @datsun_zcar.set_ship_to_phone
- #    @datsun_zcar.set_car_owner_fax
- #    @datsun_zcar.set_ship_to_fax
- # end
+ def set_datsun_zcar
+    @datsun_zcar = DatsunZcar.find(params[:id])
+    @datsun_zcar.set_datsun_zcar
+    @datsun_zcar.set_tach_calibration
+    @datsun_zcar.set_tach_calibration_without_smog
+    @datsun_zcar.set_ramshorn_w_smog
+    @datsun_zcar.set_ramshorn_w_egr_smog
+    @datsun_zcar.set_driveshaft_except_t400
+    @datsun_zcar.set_driveshaft_t400
+    @datsun_zcar.set_driveshaft_adaptor
+    @datsun_zcar.set_radiator
+    @datsun_zcar.set_electric_fan
+    @datsun_zcar.set_electrolock_t700
+    @datsun_zcar.set_subtotal
+    @datsun_zcar.set_sales_tax
+    @datsun_zcar.set_total
+    @datsun_zcar.set_same_shipping
+    @datsun_zcar.set_home_phone
+    @datsun_zcar.set_work_phone
+    @datsun_zcar.car_owner_home_phone
+    @datsun_zcar.set_ship_to_phone
+    @datsun_zcar.set_car_owner_fax
+    @datsun_zcar.set_ship_to_fax
+
+ end
 end
--- app/models/datsun_zcar.rb
+++ app/models/datsun_zcar.rb
@@ -1,2 +1,172 @@
 class DatsunZcar < ActiveRecord::Base
+  validates :vin, :car_owner_full_name, :car_owner_address, :car_owner_city, :car_owner_state, :car_owner_zip, presence: true
+
+
+def set_datsun_zcar
+  self.kit_amt = 695
+  self.save
+end
+
+def set_tach_calibration
+  if self.tach_calibration == true
+     self.tach_calibration_amt = 55
+   else
+      self.tach_calibration_amt = 0
+  end
+  self.save!
+end
+
+def set_tach_calibration_without_smog
+  if self.tach_calibration_without_smog == true
+     self.tach_calibration_without_smog_amt = 125
+   else
+      self.tach_calibration_without_smog_amt = 0
+  end
+  self.save!
+end
+
+def set_ramshorn_w_smog
+  if self.ramshorn_w_smog == true
+     self.ramshorn_w_smog_amt = 275
+   else
+      self.ramshorn_w_smog_amt = 0
+  end
+  self.save!
+end
+
+def set_ramshorn_w_egr_smog
+  if self.ramshorn_w_egr_smog == true
+     self.ramshorn_w_egr_smog_amt = 325
+   else
+      self.ramshorn_w_egr_smog_amt = 0
+  end
+  self.save!
+end
+
+def set_driveshaft_except_t400
+  if self.driveshaft_except_t400 == true
+     self.driveshaft_except_t400_amt = 165
+   else
+      self.driveshaft_except_t400_amt = 0
+  end
+  self.save!
+end
+
+def set_driveshaft_t400
+  if self.driveshaft_t400 == true
+     self.driveshaft_t400_amt = 205
+   else
+      self.driveshaft_t400_amt = 0
+  end
+  self.save!
+end
+
+def set_driveshaft_adaptor
+  if self.driveshaft_adaptor == true
+     self.driveshaft_adaptor_amt = 100
+   else
+      self.driveshaft_adaptor_amt = 0
+  end
+  self.save!
+end
+
+def set_radiator
+  if self.radiator == true
+     self.radiator_amt = 245
+   else
+      self.radiator_amt = 0
+  end
+  self.save!
+end
+
+def set_electric_fan
+  if self.electric_fan == true
+     self.electric_fan_amt = 94.25
+   else
+      self.electric_fan_amt = 0
+  end
+  self.save!
+end
+
+def set_electrolock_t700
+  if self.electrolock_t700 == true
+     self.electrolock_t700_amt = 85
+   else
+      self.electrolock_t700_amt = 0
+  end
+  self.save!
+end
+
+def set_subtotal
+  self.subtotal = self.electrolock_t700_amt + self.electric_fan_amt + self.kit_amt + self.tach_calibration_amt + self.tach_calibration_without_smog_amt + self.ramshorn_w_smog_amt + self.ramshorn_w_egr_smog_amt + self.driveshaft_except_t400_amt + self.driveshaft_t400_amt + self.driveshaft_adaptor_amt + self.radiator_amt - self.credit_amt
+  self.save!
+end
+
+def set_sales_tax
+  if self.ship_to_state == "Texas"
+    self.sales_tax = (self.subtotal) * (0.0825)
+    self.sales_tax = self.sales_tax.round(2)
+  else
+    self.sales_tax = 0
+  end
+    self.save!
+end
+
+def set_total
+  self.total = self.subtotal + self.sales_tax
+  self.save
+end
+
+def set_same_shipping
+  if self.same_information == true
+    self.ship_to_full_name = self.car_owner_full_name
+    self.ship_to_address = self.car_owner_address
+    self.ship_to_city = self.car_owner_city
+    self.ship_to_state = self.car_owner_state
+    self.ship_to_zip = self.car_owner_zip
+    self.ship_to_country = self.car_owner_country
+    self.ship_to_email = self.car_owner_email
+    self.ship_to_phone = self.car_owner_home_phone
+    self.ship_to_fax = self.car_owner_fax
+else
+  end
+  self.save
+end
+
+def set_home_phone
+  if self.car_owner_home_phone.present?
+    self.car_owner_home_phone.gsub!(/[^0-9]/, '')
+    self.car_owner_home_phone.insert(3, '-').insert(7, '-')
+  end
+end
+
+def set_work_phone
+    if self.car_owner_work_phone.present?
+      self.car_owner_work_phone.gsub!(/[^0-9]/, '')
+      self.car_owner_work_phone.insert(3, '-').insert(7, '-')
+    end
+end
+
+def set_ship_to_phone
+    if self.ship_to_phone.present?
+      self.ship_to_phone.gsub!(/[^0-9]/, '')
+      self.ship_to_phone.insert(3, '-').insert(7, '-')
+    end
+end
+
+def set_car_owner_fax
+    if self.car_owner_fax.present?
+      self.car_owner_fax.gsub!(/[^0-9]/, '')
+      self.car_owner_fax.insert(3, '-').insert(7, '-')
+    end
+end
+
+def set_ship_to_fax
+    if self.ship_to_fax.present?
+      self.ship_to_fax.gsub!(/[^0-9]/, '')
+      self.ship_to_fax.insert(3, '-').insert(7, '-')
+    end
+end
+
+
 end
